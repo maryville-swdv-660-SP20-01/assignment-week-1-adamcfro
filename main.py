14c679e41324238a79cfc62e123ff7a0ed0e9156
Adam Froehlich <adamcfro@gmail.com>
2020-01-19 10:47:09 -0500

    Added comments

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
+# import dependencies
 import emoji
 import pyfiglet
 
+# Create a banner that displays a welcome message
 banner = pyfiglet.figlet_format("Hello there!")
-print(banner)
 
-print("Welcome to the Body Mass Index Calculator!\n")
+print(f"{banner}\nWelcome to the Body Mass Index Calculator!\n")
 
+# get weight and inches to compute BMI
 weight = int(input("What is your weight in pounds? "))
 inches = int(input("What is your height in inches? "))
+
 bmi = (703 * weight) / (inches ** 2)
 
-if bmi < 18.5:
-    your_emoji = emoji.emojize(":thumbs_down:")
-elif bmi >= 18.5 and bmi <= 24.9:
-    your_emoji = emoji.emojize(":thumbs_up:")
-elif bmi >= 25 and bmi <= 29.9:
-    your_emoji = emoji.emojize(":thumbs_down:")
+# if BMI is good give a thumbs up, otherwise give thumbs down
+if bmi >= 18.5 and bmi <= 24.9:
+    emoji_symbol = emoji.emojize(":thumbs_up:")
 else:
-    your_emoji = emoji.emojize(":thumbs_down:")
+    emoji_symbol = emoji.emojize(":thumbs_down:")
 
-print("\nBMI Calculations:\n\nUnderweight: BMI < 18.5\nNormal weight: BMI 18.5 - 24.9\nOverweight: BMI 25 - 29.9\nObesity: BMI > 30")
-
-print(f"\nYour BMI is {bmi:.2f} {your_emoji}")
+# display BMI calculations
+print(
+    f"\nBMI Calculations:\n\nUnderweight: BMI < 18.5\nNormal weight: BMI 18.5 - 24.9\nOverweight: BMI 25 - 29.9\nObesity: BMI > 30\nYour BMI is {bmi:.2f} {emoji_symbol}")
 
 input("\nPress any button to exit.")
